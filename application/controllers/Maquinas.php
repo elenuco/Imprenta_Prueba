8c9fbec7a5d76aa189755b9779aa0a5361744fe8
Evelyn Elena Campos <elenuco36@gmail.com>
2023-02-08 16:59:42 -0600

    Feat: cambios pero con errores

--- application/controllers/Maquinas.php
+++ application/controllers/Maquinas.php
@@ -3,13 +3,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Maquinas extends CI_Controller{
     public function __construct() {
 		parent:: __construct();
+        //$this->load->database();
 		$this->load->model('Maquinaria_model');
       
 
 	}
 
     public function index(){
-        $data["maquinaria"]= $this->Maquinari_model->mostrar
+        $data["maquinaria"]= $this->Maquinaria_model->mostrar_maquinaria();
         $this->load->view('Maquinaria', $data);
     }
 }
--- application/models/Maquinaria_model.php
+++ application/models/Maquinaria_model.php
@@ -1,9 +1,13 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Maquinaria_model extends CI_Controller{
-public function mostrar_consultorio() {
-    $maquina = $this->db->get('maquina');
-    return $maquina->result();
+    public function __construct()
+    {
+        $this->load->database();
+    }
+public function mostrar_maquinaria() {
+    $maquinaria = $this->db->get('maquinaria');
+    return $maquinaria->result();
 }
 }
 ?>
