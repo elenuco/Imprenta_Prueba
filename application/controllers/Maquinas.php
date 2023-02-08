4c074728d01a1752eb8d923ab86697981f0d0602
elenuco <elenuco36@gmail.com>
2023-02-08 13:59:56 -0600

    Feat: cambios en los archivos

---
+++ application/controllers/Maquinas.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Maquinas extends CI_Controller{
+    public function __construct() {
+		parent:: __construct();
+		$this->load->model('Maquinaria_model');
+      
+
+	}
+
+    public function index(){
+        $data["maquinaria"]= $this->Maquinari_model->mostrar
+        $this->load->view('Maquinaria');
+    }
+}
+?>
---
+++ application/views/Maquinaria.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Maquinarias</title>
+</head>
+<body>
+    <header>
+        <nav>
+
+        </nav>
+    </header>
+    <table>
+        <thead>
+            <tr></tr>
+            <tr>
+                <th>#</th>
+                <th>Codigo</th>
+                <th>Nombre</th>
+                <th>Tipo</th>
+                <th>Descripcion</th>
+            </tr>
+        </thead>
+        <tbody>
+            
+        </tbody>
+    </table>
+</body>
+</html>
